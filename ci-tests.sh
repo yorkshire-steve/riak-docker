89b431fc6c1a071b4b6041ab1533c534405807f2
Steve Wood <steve_wood16@hotmail.com>
2021-04-24 14:28:40 +0100

    change wiat for riak

--- ci-tests.sh
+++ ci-tests.sh
@@ -8,7 +8,7 @@ pushd $BASE_DIR
 DOCKER_IMAGE_ID=$(docker build -q -t riak docker-riak)
 DOCKER_ID=$(docker run -d -p 8098:8098 riak)
 echo "Waiting for Riak to fully start..."
-sleep 20
+docker exec $DOCKER_ID riak-admin wait-for-service riak_kv
 python -m unittest discover -s src -p *_test.py
 docker stop $DOCKER_ID
 docker rm $DOCKER_ID
